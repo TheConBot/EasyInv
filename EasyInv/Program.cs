31252c4abeee82657b396c85d0ec024fca3753a6
Connor Botts <connor.botts@gmail.com>
2017-06-01 12:55:16 -0400

    Add project files.

---
+++ EasyInv/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyInv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
