c52f6b16cb7dd2a4945b89c31167d79350ee388d
Connor Botts <connor.botts@gmail.com>
2017-06-07 11:53:26 -0400

    Initial Commit

---
+++ EasyInv/InventoryAssistant.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace EasyInv
+{
+    public static class InventoryAssistant
+    {
+        private struct APIInformation
+        {
+            public const string upcBuffer = "upcCode=";
+            public const string sigBuffer = "&signature=";
+
+            public const string fieldName = "&field_names=description";
+            public const string language = "&language=en";
+            public const string appKey = "&app_key=//y3i+dH0mmg";
+            public const string authKey = "Yh28K7l2a5Kg4Zv1";
+            public const string apiUrl = "https://www.digit-eyes.com/gtin/v2_0/?";
+
+            public const char lineBreakDelimiter = ',';
+            public const char lineSeperatorDetlimiter = ':';
+            public const string lineTag = "description";
+        }
+
+        private static HttpClient client;
+        private static string itemTitle;
+
+        private static void SetNewItem(string item)
+        {
+            itemTitle = item;
+        }
+
+        public static void GetItemInformation(long upc, out string info)
+        {
+            client = new HttpClient();
+            RunAsync(upc).Wait();
+            info = itemTitle;
+        }
+
+        private static async Task RunAsync(long upc)
+        {
+            client.BaseAddress = new Uri("http://localhost:55268/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                string url = $"{APIInformation.apiUrl}{APIInformation.upcBuffer}{upc.ToString()}{APIInformation.fieldName}{APIInformation.language}{APIInformation.appKey}{APIInformation.sigBuffer}{SigningSignature(upc.ToString(), APIInformation.authKey)}";
+
+                string item = await GetInventoryItemAsync(url);
+                SetNewItem(item);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static async Task<string> GetInventoryItemAsync(string path)
+        {
+            string itemContents = "NULL";
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                itemContents = await response.Content.ReadAsStringAsync();
+                itemContents = GetTitleFromJSON(itemContents);
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Could not retrieve object with that UPC.");
+            }
+            return itemContents;
+        }
+
+        private static string SigningSignature(string upc, string auth)
+        {
+            var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(auth));
+            var m = hmac.ComputeHash(Encoding.UTF8.GetBytes(upc));
+            return Convert.ToBase64String(m);
+        }
+
+        private static string GetTitleFromJSON(string json)
+        {
+            string title = json.Replace("\"", "").Replace("}", "").Replace("{", "").Split(APIInformation.lineBreakDelimiter).Single(s => s.Contains(APIInformation.lineTag)).Replace("\n", "");
+            title = title.Substring(title.IndexOf(APIInformation.lineSeperatorDetlimiter)).Replace(APIInformation.lineSeperatorDetlimiter, ' ').Trim();
+            return title;
+        }
+    }
+}
--- EasyInv/Program.cs
+++ EasyInv/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.IO;
+using JitBit.Util;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyInv
 {
@@ -10,6 +9,65 @@ namespace EasyInv
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Write("Please enter a UPC, a CSV Path, or type quit: ");
+                string input = Console.ReadLine();
+
+                if (input.ToLower().StartsWith("quit"))
+                {
+                    break;
+                }
+                else if (long.TryParse(input, out long upc))
+                {
+                    InventoryAssistant.GetItemInformation(upc, out string info);
+                    Console.WriteLine(info);
+                }
+                else
+                {
+                    string document = "";
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(input))
+                        {
+                            document = sr.ReadToEnd();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"ERROR: Something went wrong with your input.\n{e}");
+                        continue;
+                    }
+
+                    string[] lines = document.Split('\n');
+                    string[] entrys;
+                    List<CsvExport> csv = new List<CsvExport>();
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        entrys = lines[i].Split(',');
+                        if (long.TryParse(entrys[0], out upc))
+                        {
+                            InventoryAssistant.GetItemInformation(upc, out string output);
+                            Console.WriteLine($"Writing Item to Spreadsheet: {output}");
+                            var newEntry = new CsvExport();
+                            newEntry.AddRow()
+                            newEntry[]
+                            //entrys[0] = output;
+                            //string rejoinedLine = String.Join(",", entrys);
+                            //lines[i] = rejoinedLine;
+                        }
+                    }
+                    Console.Write("Please enter a path to write the new CSV to: ");
+                    string newPath = Console.ReadLine();
+                    File.WriteAllLines(newPath, lines);
+                }
+            }
         }
+    }
+
+    class InventoryItem
+    {
+        public string title;
+
     }
 }
